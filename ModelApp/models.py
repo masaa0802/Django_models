dfa8c2c5531d225e7c009daab7e14b155923578b
Your Name <masaakigoto0802@gmail.com>
2021-12-26 17:21:15 +0900

    ~116

--- ModelApp/models.py
+++ ModelApp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
+import pytz
 
 class BaseMeta(models.Model):
-  create_at = models.DateTimeField(default=timezone.datetime.now)
-  update_at = models.DateTimeField(default=timezone.datetime.now)
+  create_at = models.DateTimeField(default=timezone.datetime.now(pytz.timezone('Asia/Tokyo')))
+  update_at = models.DateTimeField(default=timezone.datetime.now(pytz.timezone('Asia/Tokyo')))
 
   class Meta:
     abstract = True
@@ -17,11 +18,13 @@ class Person(BaseMeta):
   memo = models.TextField()
   web_site = models.URLField(null=True,blank=True)
 
-class Meta:
-  db_table = 'person'
-  index_together = [['first_name', 'last_name']]
-  ordering = ['salary']
+  class Meta:
+    db_table = 'person'
+    index_together = [['first_name', 'last_name']]
+    ordering = ['salary']
 
+  def __str__(self):
+    return f'{self.first_name} {self.last_name}'
 
 
 
