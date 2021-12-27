a006453d6287e918ef00c078759d50db8e336eae
Your Name <masaakigoto0802@gmail.com>
2021-12-27 22:35:29 +0900

    ~120

--- ModelApp/models.py
+++ ModelApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.query import Prefetch
 from django.utils import timezone
 import pytz
 
@@ -26,6 +27,31 @@ class Person(BaseMeta):
   def __str__(self):
     return f'{self.first_name} {self.last_name}'
 
+class Students(models.Model):
+  name = models.CharField(max_length=20)
+  age = models.IntegerField()
+  major = models.CharField(max_length=20)
+  school = models.ForeignKey(
+    'Schools', on_delete=models.RESTRICT 
+  )
+  prefecture = models.ForeignKey(
+    'Prefectures', on_delete=models.CASCADE
+  )
+  class Meta:
+    db_table = 'students'
+
+class Schools(models.Model):
+  name = models.CharField(max_length=20)
+  prefecture = models.ForeignKey(
+    'Prefectures', on_delete=models.CASCADE
+  )
+  class Meta:
+    db_table = 'schools'
+
+class Prefectures(models.Model):
+  name = models.CharField(max_length=20)
+  class Meta:
+    db_table = 'prefectures'
 
 
 
